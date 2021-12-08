c889f97140c95e439a4bd90bb7cab74539925dce
Rosineia <neyasantoss@gmail.com>
2021-12-07 21:17:15 -0300

    Atualização exercicios C#

--- Exercicios/MomentoOnline02.12/Pessoa.cs
+++ Exercicios/MomentoOnline02.12/Pessoa.cs
@@ -2,33 +2,67 @@ using System;
 
 namespace MomentoOnline02._12
 {
-public class Pessoa{
+    public class Pessoa
+    {
 
-// bloco dos ATRIBUTOS ou propriedades ou 
-public string nome;
+        // bloco dos ATRIBUTOS ou propriedades ou 
+        public string nome;
 
-public double altura, peso;
+        public double altura, peso;
 
-public DateTime data_nasc;
-
-
-
-// bloco dos METODOS ou funcionalidades
+        public DateTime data_nasc;
 
 
-public void acordar(){
 
-}
+        // bloco dos METODOS ou funcionalidades
 
-public void caminhar(){
-    
-}
 
-public void dormir(){
-    
-}
+        public void acordar()
+        {
 
-}
+            Console.WriteLine("Acordando! " + nome + " Bom dia!!");
+        }
+
+        public void caminhar()
+        {
+
+            Console.WriteLine(nome + "Iniciando Caminhada");
+
+        }
+
+        public void dormir()
+        {
+
+            Console.WriteLine(nome + " Dormindo! Boa noite!!");
+
+        }
+
+        public void exibirinformacoes()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Peso: " + peso);
+            Console.WriteLine("Altura: " + altura);
+            Console.WriteLine("Data de nascimento: " + data_nasc);
+
+        }
+        public void gerarSenha()
+        {
+
+            int idade;
+            string senha;
+            idade = DateTime.Now.Year - data_nasc.Year;
+
+            if(idade>18){
+                senha = nome+idade;
+
+            }else{
+                senha = idade + nome;
+                
+            }
+            Console.WriteLine("Senha:" + senha);
+            Console.ReadLine();
+        }
+    }
 
 
 
--- Exercicios/MomentoOnline02.12/Program.cs
+++ Exercicios/MomentoOnline02.12/Program.cs
@@ -8,15 +8,26 @@ namespace MomentoOnline02._12
         {
             Pessoa pele = new Pessoa();// tipo de dado; variavel(objeto criado); new; construtor utilizado para instanciar o objeto
             Pessoa neymar = new Pessoa();
-            Pessoa xuca = new Pessoa();
+            Pessoa xuxa = new Pessoa();
 
+            // acessando atributos classe pessoas
             pele.nome = "Edson";
             pele.peso = 80.10;
             pele.altura = 1.86;
-            pele.data_nasc= 1950-12-02;
+            pele.data_nasc = DateTime.Parse ("1985 - 12 - 02");
+
+            // Atribuir inf. aos atributos do objeto
+            pele.nome = "Edson";
+            xuxa.nome = "Maria da Graça";
+            neymar.nome = "Neymar Jr.";
 
 
 
+            // acessando os metodos da classe pessoa
+            neymar.acordar();
+            xuxa.acordar();
+            pele.exibirinformacoes();
+            pele.gerarSenha();
 
 
         }
