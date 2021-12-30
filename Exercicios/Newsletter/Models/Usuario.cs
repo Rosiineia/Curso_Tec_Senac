18a5432fbcac669d1d2f103b0aaa24df3921354c
Rosineia <neyasantoss@gmail.com>
2021-12-29 22:34:54 -0300

    Exercicio Newsletter

---
+++ Exercicios/Newsletter/Models/Usuario.cs
@@ -0,0 +1,11 @@
+namespace Newsletter.Models
+{
+    public class Usuario
+    {
+        public string nome {get;set;}
+
+        public string genero { get; set;}
+        
+        public string email {get; set;}
+    }
+}
