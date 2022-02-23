eabdf862257995e8e1c91ccdf87d8aaf1c998550
Rosineia <neyasantoss@gmail.com>
2022-02-23 07:31:26 -0300

    UC 04 atividade 2 - Pcotes Turisticos

---
+++ MODULO 01/Exercicios/UC04_Atividade2/Controllers/PacotesTuristicosController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Rosineia_UC04_Atividade2.Models;
+
+
+namespace UC04_Atividade2.Controllers
+{
+    public class PacotesTuristicosController : Controller
+    {
+        //CRUD DE PACOTES TURISTICOS
+    }
+}
---
+++ MODULO 01/Exercicios/UC04_Atividade2/Models/PacotesTuristicosRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using MySqlConnector;
+
+namespace UC04_Atividade2.Models
+{
+    public class PacotesTuristicosRepository
+    {
+        //CRUD + BUSCAR PO iD
+    }
+}
