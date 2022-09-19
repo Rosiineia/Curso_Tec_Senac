aeec7b5763444b26f6a56d54b7daa9ebe67870f3
Rosineia <neyasantoss@gmail.com>
2022-09-19 15:12:33 -0300

    Atividade relogio finalizada

--- MODULO3/MO25.08_UC10/Relogio/App.js
+++ MODULO3/MO25.08_UC10/Relogio/App.js
@@ -16,12 +16,22 @@ export default class App extends Component{
     this.state={
       data: new Date(),
       textostatus:'Desativado'
+      //contagem: 0 - prox atividade
     }
     this.liga = this.liga.bind(this)
+    this.idTimer = null
   }
 
-  liga() {
-    this.setState({textostatus:'Ativado'})
+  liga() {                                    ///id - setInterval(metodo a ser executado(), intervalo de tempo em milisegundos) 
+                                                //clearInterval(id) 
+    if(this.idTimer != null){
+      clearInterval(this.idTimer)
+      this.idTimer = null
+      this.setState({textostatus:'Desativado'})// this.textoBotão.ligar
+    }else{
+      this.setState({textostatus:'Ativado', })// this.textoBotão.pausar
+    this.idTimer= setInterval(()=>{this.setState({data : new Date()})} , 1000)//contagem(this.contahem+ 1)
+    }    
   }
   render(){
     return(
@@ -29,7 +39,7 @@ export default class App extends Component{
         <Text style={{color:'red',fontSize:25,fontWeight:'bold',
     textAlign:'center', padding:40 }}> Relógio </Text>
     <Text style={{color:'blue',fontSize:35,fontWeight:'bold',
-    textAlign:'center', padding:10 }}>{this.state.data.toLocaleTimeString()}</Text>
+    textAlign:'center', padding:10 }}>{this.state.data.toLocaleTimeString()}s</Text>
     <Text style={{color:'gray',fontSize:15,fontWeight:'bold',
     textAlign:'center', padding:10 }}> {this.state.textostatus} </Text>
     <View style={{flexDirection:'row'}}>
